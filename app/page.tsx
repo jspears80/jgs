34eb1692c60715be031c9e38e20710bae6c57d8f
JCC <jspears80@gmail.com>
2025-10-09 21:12:03 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import React from 'react';
 export default function HomePage() {
   return (
     <section className="section container">
-      <h1 className="title">Home Page</h1>
-      <p>Welcome to the Home Page!</p>
+      <div className="page home-grid">
+       
+      </div>
     </section>
   );
 }
-
